97ef860ac65a2ad9453a8eaa83b9eb66dd064b2e
Kartik Rajesh <kartik70rajesh@yahoo.co.in>
2019-02-28 01:32:15 +0530

    new arch

--- num.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-matrix1 = np.array([[2,3],[3,4]])
-
-print(matrix1)
-
-arr2=np.arange(12)
-arr3=arr2.reshape([3,4])
-print(arr2)
-print(arr3)
--- take69.py
+++ take69.py
@@ -1,58 +1,65 @@
 import csv
-
-dataset = pd.read_csv("articles1.csv")
-dataset= dataset.drop(columns=['id','url']
-
-partitions = {
-    #Input features
-        'predictor': {
-            'email': {
-                'dataset': {
-                    'original': dataset.a_body,
-                    'cleaned': None,
-                    'vectorised': None,
-                    'training': None,
-                    'testing': None
-                },
-                'tfidf': {}
-            },
-        },
-        #Targets 
-        'label': {
-            'queue': {
-                'dataset': {
-                    'original': dataset.queue,
-                    'vectorised': None,
-                    'training': None,
-                    'testing': None
-                },
-                'model': {},
-                'accuracy': {},
-                'label_encoder': {}
-            },
-            'type': {
-                'dataset': {
-                    'original': dataset.type,
-                    'vectorised': None,
-                    'training': None,
-                    'testing': None
-                },
-                'model': {},
-                'accuracy': {},
-                'label_encoder': {}
-            },
-            'priority': {
-                'dataset': {
-                    'original': dataset.priority,
-                    'vectorised': None,
-                    'training': None,
-                    'testing': None
-                },
-                'model': {},
-                'accuracy': {},
-                'label_encoder': {}
+import pandas as pd
 
 
-            }
-        }
-}
+dataset1 = pd.read_csv("Truefinal.csv",encoding = 'unicode_escape')
+dataset2 = pd.read_csv("Fakefinal.csv",encoding = 'unicode_escape')
+
+dataset1 = dataset1.drop(columns=[''])
+
+
+
+#partitions = {
+#     #Input features
+#         'predictor': {
+#             'email': {
+#                 'dataset': {
+#                     'original': dataset.a_body,
+#                     'cleaned': None,
+#                     'vectorised': None,
+#                     'training': None,
+#                     'testing': None
+#                 },
+#                 'tfidf': {}
+#             },
+#         },
+#         #Targets 
+#         'label': {
+#             'queue': {
+#                 'dataset': {
+#                     'original': dataset.queue,
+#                     'vectorised': None,
+#                     'training': None,
+#                     'testing': None
+#                 },
+#                 'model': {},
+#                 'accuracy': {},
+#                 'label_encoder': {}
+#             },
+#             'type': {
+#                 'dataset': {
+#                     'original': dataset.type,
+#                     'vectorised': None,
+#                     'training': None,
+#                     'testing': None
+#                 },
+#                 'model': {},
+#                 'accuracy': {},
+#                 'label_encoder': {}
+#             },
+#             'priority': {
+#                 'dataset': {
+#                     'original': dataset.priority,
+#                     'vectorised': None,
+#                     'training': None,
+#                     'testing': None
+#                 },
+#                 'model': {},
+#                 'accuracy': {},
+#                 'label_encoder': {}
+
+
+#             }
+#         }
+# }
+
